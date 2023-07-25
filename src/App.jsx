25761e0e207ce908835c7e205fec26985a66b660
Saurav Kalia <sauravhunny@gmail.com>
2023-07-25 19:35:25 +0530

    build issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,30 @@ import {
   Hero,
   Navbar,
   Tech,
-  Wroks,
+  Works,
   StarsCanvas,
 } from "./components";
 
 const App = () => {
-  return <div>
+  return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar/>
-        <Hero/>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      </div>
-      </BrowserRouter>
-    </div>;
+    </BrowserRouter>
+  );
 };
 
 export default App;
